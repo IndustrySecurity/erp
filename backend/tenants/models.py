8b11062ce4a47dcdcf1e6aa90a3e8618a65f25a4
IndustrySecurity <30697286+IndustrySecurity@users.noreply.github.com>
2023-11-18 17:44:40 +0800

    updated version

--- backend/tenants/models.py
+++ backend/tenants/models.py
@@ -1,6 +1,15 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+from django_tenants.models import TenantMixin
+
+class EnterpriseTenant(TenantMixin):
+    tenant_number = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    contact_email = models.EmailField()
+    # 添加其他租户字段
+
+    auto_create_schema = True  # 自动创建数据库模式
+
+    def __str__(self):
+        return self.tenant_number
